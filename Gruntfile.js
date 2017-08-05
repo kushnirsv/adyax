8cf1d81af0bae4b6b02060d4c7e5e6d8a819ceaf
kushnirsv <kushnirsv92@gmail.com>
2017-08-05 14:28:12 +0300

    version 1.0

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        outputStyle: 'compressed',
+        sourceMap: true
+      },
+      all: {
+        files: {
+          'css/application.css': 'scss/application.scss'
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer-core')({
+            browsers: ['> 0.5%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'ie >6']
+          })
+        ]
+      },
+      all: {
+        src: 'css/*.css'
+      }
+    },
+
+    uglify: {
+      options: {
+        sourceMap: true
+      },
+      vendor: {
+        files: {
+          'js/vendor.min.js': [
+            'js/vendor/jquery.min.js',
+            'js/jquery-filestyle.min.js'
+          ]
+        }
+      },
+      custom: {
+        files: {
+          'js/app.min.js': [
+            'js/custom/*.js',
+            'js/custom/*/*.js'
+          ]
+        }
+      }
+    },
+
+    imagemin: {
+      all: {
+        files: [{
+          expand: true,
+          cwd: 'images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'images/'
+        }]
+      }
+    },
+
+    watch: {
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass', 'postcss']
+      },
+      jsVendor: {
+        files: 'js/vendor/**/*.js',
+        tasks: ['uglify:vendor']
+      },
+      jsCustom: {
+        files: 'js/custom/**/*.js',
+        tasks: ['uglify:custom']
+      },
+      system: {
+        files: ['Gruntfile.js', 'package.json']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['sass', 'postcss', 'imagemin', 'uglify']);
+
+};
---
+++ js/custom/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function($){
+    $(function(){
+        $(".jfilestyle").jfilestyle();
+    });
+})(jQuery);
